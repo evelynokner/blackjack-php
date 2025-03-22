35377e4f652eb2915467a0a30c9325caf8207b80
evelynokner <oknerevelyn@gmail.com>
2025-03-22 14:26:45 -0400

    Update blackjack.php

--- blackjack.php
+++ blackjack.php
@@ -16,6 +16,15 @@ $card_deck[52] = [2, 2, 2, 2,
                     11, 11, 11, 11]; #Ace
                     
 # ask user if they wish to play game, if true, call play(), else quit
+echo "Welcome to the casino! Would you like to play a round of Blackjack?\n";
+# boolean to play or quit
+$play_game = false;
+$play_or_quit = readline("Y for yes or N for no: ");
+if($play_or_quit == "Y"){
+    $play_game = true;
+}
+else { $play_game = false; }
+
 function play(){}
 
 function deal_cards(){}
