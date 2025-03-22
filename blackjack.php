33e8ca3662bc823b047e419ba01a150c3e35d59c
evelynokner <oknerevelyn@gmail.com>
2025-03-22 15:05:54 -0400

    Update blackjack.php

--- blackjack.php
+++ blackjack.php
@@ -16,17 +16,56 @@ $card_deck[52] = [2, 2, 2, 2,
                     11, 11, 11, 11]; #Ace
                     
 # ask user if they wish to play game, if true, call play(), else quit
-echo "Welcome to the casino! Would you like to play a round of Blackjack?\n";
+echo "Welcome to the casino! Would you like to play Blackjack?\n";
 # boolean to play or quit
-$play_game = false;
-$play_or_quit = readline("Y for yes or N for no: ");
-if($play_or_quit == "Y"){
-    $play_game = true;
+
+# if prompt returns true, play/continue game, else, end game
+while(prompt()){
+    play();    
 }
-else { $play_game = false; }
+stop();
 
-function play(){}
+function prompt(){
+    # returns true if user says Y, else ends game
+    return ("Y" == readline("Y for yes or N for no: "));
+}
 
-function deal_cards(){}
+function play(){
+    if (deal_cards()){
+        # blackjack
+    } else
+    {
+    
+    }
+    # if player does not have 21 pts, dealer moves
+    if (!player_move()){
+        dealer_move();
+    }
+    # TODO: track points
+    show_results();
+    echo "playing\n";
+}
+
+function stop(){
+    echo "Thanks for playing!";
+}
+
+function deal_cards(){
+    echo "Dealing cards\n";
+    # if current hand's points = 21, blackjack
+    if(blackjack()){
+       return true; 
+    } else return false;
+}
+
+function player_move(){
+    echo "Player's turn\n";
+
+}
+
+function dealer_move(){
+    echo "Dealer's turn\n";
+    
+}
 
 ?>
