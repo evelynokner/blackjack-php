ceb3c289c3621274f301caada4d3792469c680b8
evelynokner <oknerevelyn@gmail.com>
2025-03-23 17:54:36 -0400

    Update blackjack.php

--- blackjack.php
+++ blackjack.php
@@ -1,8 +1,7 @@
 <?php
-# https://www.programiz.com/php/online-compiler/
 # 52 card deck
 # randomly pick 2 cards from deck for player & dealer when round starts
-$card_deck = array(2, 2, 2, 2, 
+$card_deck = array(2, 2, 2, 2,
                     3, 3, 3, 3,
                     4, 4, 4, 4,
                     5, 5, 5, 5,
@@ -16,6 +15,11 @@ $card_deck = array(2, 2, 2, 2,
                     10, 10, 10, 10, #King
                     11, 11, 11, 11); #Ace
 
+$ranks = ["Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King", "Ace"];
+$suits = ["diamonds", "hearts", "spades", "clubs"];
+
 $blackjack = false;
 $player_hand = array();
 $dealer_hand = array();
@@ -26,7 +30,8 @@ echo "Welcome to the casino! Would you like to play Blackjack?\n";
 
 # if prompt returns true, play/continue game, else, end game
 while(prompt()){
-    play();    
+    play();
+    # TODO: restore deck to original state (52 cards)
 }
 stop();
 
@@ -36,7 +41,7 @@ function prompt(){
 }
 
 function play(){
-    deal_cards();
+    deal_cards_first_time();
     player_move();
     # if player does not automatically get a blackjack, dealer moves
     if (!$blackjack){
@@ -51,26 +56,31 @@ function stop(){
     echo "Thanks for playing!";
 }
 
-function deal_cards(){
+function deal_cards_first_time(){
     echo "Dealing cards\n";
 
     global $player_hand, $dealer_hand;
-    deal_card($player_hand);
-    deal_card($player_hand);
-    
-    deal_card($dealer_hand);
-    deal_card($dealer_hand);
+    array_push($player_hand, deal_card());
+    array_push($player_hand, deal_card());
+   
+    array_push($dealer_hand, deal_card());
+    array_push($dealer_hand, deal_card());
 
-    echo "Player hand: ", $player_hand[0], "\n";
+    echo "Player hand: ", print_hand($player_hand), "\n";
+    echo "Dealer hand: ", print_hand($dealer_hand), "\n";
+
 }
 
-function deal_card($hand){
+function deal_card(){
     global $card_deck;
-    $card = array_rand($card_deck, 1);
-    return array_push($hand, $card);
-    
-    
-    
+    $random_card = array_rand($card_deck, 1);
+    if ($card_deck[$random_card] == 0) { // taken, try again
+        deal_card();
+    } else
+    {
+        $card_deck[$random_card] = 0; // mark the card as taken
+        return $random_card;
+    }
 }
 
 function player_move(){
@@ -82,13 +92,13 @@ function player_move(){
 
 function dealer_move(){
     echo "Dealer's turn\n";
-    
+   
 }
 
 function hit_or_stand(){
     # gives user option to hit (draw more cards) or stand (leave hand as it is)
-    echo "Would you like to Hit or Stand?";
-    if (readline("H for hit, S for stand") == "H"){
+    echo "Would you like to Hit or Stand?\n";
+    if (readline("H for hit, S for stand: ") == "H"){
         hit();
     } else stand();
 }
@@ -104,6 +114,25 @@ function hit(){
 
 function stand(){
     echo "User stands";
+}
+
+function print_hand($hand){
+    //echo implode(", ", $hand);
+    global $card_deck;
+    foreach ($hand as $card) {
+        
+        echo get_card_rank($card), " of ", get_card_suit($card), "\n";
+    }
+}
+
+function get_card_rank($card_position){
+    global $ranks;
+    return $ranks[floor($card_position / 4)];
+}
+
+function get_card_suit($card_position){
+    global $suits;
+    return $suits[$card_position % 4];
 }
 
 ?>
