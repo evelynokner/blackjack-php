616d8f4ce615b24e6f7932f22b8703e82d21d2a1
evelynokner <oknerevelyn@gmail.com>
2025-03-27 15:37:22 -0400

    Update blackjack.php

--- blackjack.php
+++ blackjack.php
@@ -10,48 +10,44 @@ $ranks = ["Two", "Three", "Four", "Five", "Six",
             "Seven", "Eight", "Nine", "Ten",
             "Jack", "Queen", "King", "Ace"];
 $rank_value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];            
-$suits = ["diamonds", "hearts", "spades", "clubs"];
+$suits = ["Diamonds", "Hearts", "Spades", "Clubs"];
 
-# -1 = no winner, 0 = player wins, 1 = dealer wins, 2 = tie
-$winner = -1;
 $player_hand = array();
 $dealer_hand = array();
 
+# -1 = no winner, 0 = player wins, 1 = dealer wins, 2 = tie
+$winner = -1;
 $player_points = 0;
 $dealer_points = 0;
 
-# ask user if they wish to play game, if true, call play(), else quit
-echo "Welcome to the casino! Would you like to play Blackjack?\n";
-# boolean to play or quit
+echo "Welcome to the casino! ";
 
 # if prompt returns true, play/continue game, else, end game
 while(prompt()){
     play();
-    # TODO: restore deck to original state (52 cards)
-}
-stop();
+} stop();
 
 function prompt(){
     # returns true if user says Y, else ends game
-    return ("Y" == readline("Y for yes or N for no: "));
+    return ("Y" == readline("Would you like to play Blackjack?\nY for yes or N for no: "));
 }
 
 function play(){
     initialize_round();
     deal_cards_first_time();
-    // if a player immediately gets a blackjack after first cards are dealt, end game
+    # if a player immediately gets a blackjack after first cards are dealt, end game
     if (gameover()) return;
     player_move();
     if (gameover()) return;
     dealer_move();
     show_results();
-    echo "playing\n";
 }
 
 function initialize_round(){
-    global $card_deck, $winner, $player_hand, $dealer_hand,
+    global $card_deck, $winner, 
+    $player_hand, $dealer_hand,
     $player_points, $dealer_points;
-    // reset card deck
+    # reset card deck
     $card_deck = array_fill(0, 52, 1);
     # -1 = no winner, 0 = player wins, 1 = dealer wins, 2 = tie
     $winner = -1;
@@ -72,8 +68,6 @@ function stop(){
 }
 
 function deal_cards_first_time(){
-    echo "Dealing cards\n";
-
     global $player_hand, $dealer_hand, 
     $player_points, $dealer_points;
     array_push($player_hand, deal_card(0));
@@ -82,7 +76,7 @@ function deal_cards_first_time(){
     array_push($dealer_hand, deal_card(1));
     array_push($dealer_hand, deal_card(1));
 
-    echo "Player hand:\n", print_hand($player_hand), "\n";
+    echo "\nPlayer hand:\n", print_hand($player_hand), "\n";
     echo "Dealer hand:\n", print_hand($dealer_hand), "\n";
     
     echo "Player points: ", $player_points, "\n";
@@ -92,75 +86,74 @@ function deal_cards_first_time(){
 function deal_card($move){
     # move = 0 (player's move)
     # move = 1 (dealer's move)
-    global $card_deck, $player_points, $dealer_points,
-    $winner;
+    global $card_deck, $winner,
+    $player_points, $dealer_points;
     $random_card = array_rand($card_deck, 1);
-    if ($card_deck[$random_card] == 0){ // taken, try again
+    # card taken, try again
+    if ($card_deck[$random_card] == 0){
         deal_card($move);
     } else {
-        $card_deck[$random_card] = 0; // mark the card as taken
+        $card_deck[$random_card] = 0; # mark the card as taken
         $value = get_card_points($random_card);
         if ($move == 0){
             $player_points += $value;
-            update_result(0); // update result for player
-                
+            update_result(0); # update result for player
         } else {
             $dealer_points += $value;
-            update_result(1); // update result for dealer
-        }
-        return $random_card;
+            update_result(1); # update result for dealer
+        } return $random_card;
     }
 }
 
 function update_result($side) {
-    global $player_points, $dealer_points,
-    $winner;
     $points;
+    global $player_points, $dealer_points, $winner;
+    
     if ($side == 0) $points = $player_points;
     else $points = $dealer_points;
-    
+    # tie if player & dealer both have 21 pts
     if($player_points == 21 && $dealer_points == 21){
-        $winner = 2; // tie
-        return; 
+        $winner = 2; # tie
+        return;
     }
     if($points == 21){
-        $winner = $side; // player wins
+        $winner = $side; # player wins
     }
     else if($points > 21){
-        // if points on current side is over 21, other side wins
+        # if points on current side is over 21, other side wins
         if ($side == 0) $winner = 1;
         else $winner = 0; 
     }
 }
 
 function player_move(){
-    echo "Player's turn\n";
+    echo "\nPlayer's turn:\n";
     hit_or_stand();
 }
 
 function dealer_move(){
-    // dealer draws cards until:
+    # dealer draws cards until:
     // blackjack OR bust OR points > player's points
     echo "Dealer's turn\n";
-    global $dealer_hand, $dealer_points, $player_points, $winner;
+    global $dealer_hand, $dealer_points, 
+    $player_points, $winner;
     while(true){
-        // deal cards to dealer and update hand
+        # deal cards to dealer and update hand
         array_push($dealer_hand, deal_card(1));
         if($dealer_points > 21){
-            // bust, player wins
+            # bust, player wins
             $winner = 0;
             return;
         }
-        // dealer wins if:
-        // dealer points bigger than player points
+        # dealer wins if:
+        // dealer has more points than player
         // or dealer has 21 points (blackjack)
         if($dealer_points > $player_points || $dealer_points == 21){
             $winner = 1;
             return;
         } 
-        // continue dealing cards if the above conditions have not been met yet
+        # continue dealing cards if the above conditions have not been met yet
         else array_push($dealer_hand, deal_card(1));
-
     }
 }
 
@@ -178,7 +171,7 @@ function hit(){
     # deal cards and check if game over
     array_push($player_hand, deal_card(0));
     echo "Player hand:\n", print_hand($player_hand), "\n";
-    // print player and dealer points again
+    # print player and dealer points again
     show_results();
 }
 
@@ -187,9 +180,8 @@ function stand(){
 }
 
 function print_hand($hand){
-    //echo implode(", ", $hand);
     global $card_deck;
-    foreach ($hand as $card) {
+    foreach ($hand as $card){
         echo get_card_rank($card), " of ", get_card_suit($card),
         "\nPoints: ", get_card_points($card), "\n";
     }
@@ -220,10 +212,10 @@ function show_results(){
         if($winner == 1) echo "Dealer wins!\n";
         else echo "No winner (error)\n";
     }
-    // display new hands
+    # display new hands
     echo "Player hand:\n", print_hand($player_hand), "\n";
     echo "Dealer hand:\n", print_hand($dealer_hand), "\n";
-    // display player and dealer points
+    # display player and dealer points
     echo "Player points: ", $player_points, "\n";
     echo "Dealer points: ", $dealer_points, "\n";
 }
